c33e82d77c9d0c3dc3b5e63a53fc046485ac79e1
100472175 <100472175@alumnos.uc3m.es>
2023-10-22 21:52:53 +0200

    Funciona la primera galería, la segunda vuelve siempre a la 1º

--- script/landing.js
+++ script/landing.js
@@ -1,20 +1,20 @@
 let nav = document.querySelector(".nav_menu");
 let abrir = document.querySelector(".abrir");
-let cerrar= document.querySelector(".cerrar");
+let cerrar = document.querySelector(".cerrar");
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     cerrar.style.display = 'none';
     abrir.style.display = 'block';
     nav.style.display = 'none';
 })
 
-abrir.addEventListener('click', ()=>{
+abrir.addEventListener('click', () => {
     cerrar.style.display = 'block';
     abrir.style.display = 'none';
     nav.style.display = 'block';
 
 })
-cerrar.addEventListener('click',()=>{
+cerrar.addEventListener('click', () => {
     cerrar.style.display = 'none';
     abrir.style.display = 'block';
     nav.style.display = 'none';
@@ -27,7 +27,7 @@ function getCookie(dni) {
     let clave = dni;
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
+    for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -39,7 +39,7 @@ function getCookie(dni) {
     return "";
 }
 
-function setCookie(dni,exdays) {
+function setCookie(dni, exdays) {
     let name = document.getElementById("name").value;
     comp_nombre_ape(name);
     let surname = document.getElementById("surname").value;
@@ -50,7 +50,7 @@ function setCookie(dni,exdays) {
     comp_correo(mail);
 
     const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = "dni=" + dni + "; name=" + name + "; surname=" + surname + "; tfl=" + tlf + "; mail=" + mail + "; path=/";
 }
@@ -66,8 +66,7 @@ function createAccount() {
     if (getCookie(dni_) !== "") {
         alert("Ya existe una cuenta con este DNI");
 
-    }
-    else {
+    } else {
         // Create a cookie with the information:
         setCookie(dni_, 30);
         alert("Cuenta Creada");
@@ -75,30 +74,52 @@ function createAccount() {
 
 }
 
-function comp_dni(dni_){
+function comp_dni(dni_) {
     let pattern = /^[0-9]{8}[A-Z]$/;
-    if (false == pattern.test(dni_)){
+    if (false == pattern.test(dni_)) {
         alert("DNI no valido")
     }
 
 }
 
-function comp_tlf(tlf){
+function comp_tlf(tlf) {
     let pattern = /^[0-9]{9}$/;
-    if(false == pattern.test(tlf)){
+    if (false == pattern.test(tlf)) {
         alert("Telefono no valido");
     }
 }
-function comp_correo(correo){
+
+function comp_correo(correo) {
     let pattern = /^[a-z0-9]+@[a-z]+\.[a-z]{3}$/;
-    if(false == pattern.test(correo)){
+    if (false == pattern.test(correo)) {
         alert("Correo no valido")
     }
 }
 
-function comp_nombre_ape(nom){
+function comp_nombre_ape(nom) {
     let pattern = /^[A-Z,a-z]$/;
-    if(false == pattern.test(corre)){
+    if (false == pattern.test(corre)) {
         alert("Nombre u Apellido no valido")
     }
 }
+
+
+/* Funciones para la galería de fotos */
+var slideIndex = 1;
+setInterval(function () {
+    document.getElementById("radio" + slideIndex).checked = true;
+    slideIndex++;
+    if (slideIndex > 4) {
+        slideIndex = 1;
+    }
+}, 5000);
+
+slideIndex_bebida = 1;
+setIntervalBebida(function () {
+    document.getElementById("bebida_radio" + slideIndex_bebida).checked = true;
+    slideIndex_bebida++;
+    if (slideIndex_bebida > 5) {
+        slideIndex_bebida = 1;
+    }
+}, 4000);
+
